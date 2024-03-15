a5da0c12b0e87ef26571fe560acf5d7b72669d35
Luis-Medina-ASU <lamedin4@asu.edu>
2024-03-14 21:41:09 -0700

    Initial Commit

---
+++ Target Better Price/Target_Better_PriceApp.swift
@@ -0,0 +1,20 @@
+//
+//  Target_Better_PriceApp.swift
+//  Target Better Price
+//
+//  Created by Luis Medina on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Target_Better_PriceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
